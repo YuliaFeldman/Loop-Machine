7991c3d825856a61413051e724239f3a1fdd4af8
Yulia Feldman <48710519+YuliaFeldman@users.noreply.github.com>
2021-01-01 16:28:28 +0200

    fixed synchronization problem

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 
-var paused = false;  //if pause button pressed
-var myAudio = [document.getElementById("myAudio1"),     //loop samples
+var stopped = false;  //if stop button pressed
+
+var myAudio = [document.getElementById("myAudio1"),     //audio samples
 		document.getElementById("myAudio2"),
 		document.getElementById("myAudio3"),
 		document.getElementById("myAudio4"),
@@ -10,40 +11,82 @@ var myAudio = [document.getElementById("myAudio1"),     //loop samples
 		document.getElementById("myAudio7"),
 		document.getElementById("myAudio8"),
 		document.getElementById("myAudio9")];
-var isPlaying = [false, false, false, false, false, false, false, false, false];  //states of pads
+
+var isPlaying = [false, false, false, false, false, false, false, false, false];  //states of pads in current loop
+var shouldBePlaying = [false, false, false, false, false, false, false, false, false];  //states of pads in the next loop
+
+/*
+An audio has finished playing one loop
+*/
+function playAll(id) {
+	
+	isPlaying[id] = false;
+	var otherIsPlaying = false;
+		
+	//look for another tab that played in this loop
+	for(var i = 0; i < myAudio.length; i++)
+	{
+		if(isPlaying[i])
+		{	
+			otherIsPlaying = true;
+			break;
+		}
+	}
+	
+	//if there are no other playing tabs, start playing all "waiting" tabs
+	if(!otherIsPlaying){
+		for(var i = 0; i < myAudio.length; i++)
+		{
+			if(shouldBePlaying[i])
+			{
+				myAudio[i].play();
+				isPlaying[i] = true;
+			}
+		}
+	}
+}
 
 /**
-* When a pad is turned on, it starts playing its loop
+* A pad is pressed
 */
 function togglePlay(id) {
-    const element = document.getElementById(id.toString());
-     if(isPlaying[id])
+    
+	const element = document.getElementById(id.toString());
+     
+	if(shouldBePlaying[id])  //if pad is already turned on
 	{
 		myAudio[id].pause();
+		shouldBePlaying[id] = false;
 		isPlaying[id] = false;
+		
+		
 		//change back color of pad
 		element.style.color = "#FFFFFF";
 		element.style.borderColor = "#00FFFF";
 		
 	}
-     else
+    else  //if pad is turned off
 	{
+		var otherIsPlaying = false;
 		myAudio[id].currentTime = 0;
 		
-		//sync this loop with other playing loops
+		//look for another playing pad
 		for(var i = 0; i < myAudio.length; i++)
 		{
-			if(isPlaying[i])
+			if(shouldBePlaying[i])
 			{	
-				myAudio[id].currentTime = myAudio[i].currentTime;
+				otherIsPlaying = true;
 				break;
 			}
 		}
-		if(!paused)
+		
+		if(!stopped && !otherIsPlaying)  //if there are no other playing pads, then start playing. If there are, wait for this loop to finish.
 		{
 			myAudio[id].play();
+			isPlaying[id] = true;
 		}
-		isPlaying[id] = true;
+		shouldBePlaying[id] = true;
+		
 		
 		//change color of pad
 		element.style.color = "#FDFF33";
@@ -52,37 +95,40 @@ function togglePlay(id) {
 };
 
 /**
-* Stop button is pressed. Pause all playing loops.
+* Stop button is pressed. Stop all playing audio samples.
 */
 function stopButtonPressed()
 {	
-	if(!paused)
+	if(!stopped)
 	{
 		for(var i = 0; i < myAudio.length; i++)
 		{
-			if(isPlaying[i])
+			if(shouldBePlaying[i])
 			{	
 				myAudio[i].pause();
+				myAudio[i].currentTime = 0;
+				isPlaying[i] = false;
 			}
 		}
 	}
-	paused = true;
+	stopped = true;
 };
 
 /**
-* Play button is pressed. Continue playing all paused loops.
+* Play button is pressed. Start playing all stopped audio samples.
 */
 function playButtonPressed()
 {
-	if(paused)
+	if(stopped)
 	{
 		for(var i = 0; i < myAudio.length; i++)
 		{
-			if(isPlaying[i])
+			if(shouldBePlaying[i])
 			{
 				myAudio[i].play();
+				isPlaying[i] = true;
 			}
 		}
 	}
-	paused = false;
+	stopped = false;
 };
